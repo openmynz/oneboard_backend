dde046e7365723d7cd788aaccab76b6cdf36efc0
Ravi Teja <RaviTejaJP>
2024-01-19 11:46:57 +0000

    User authentication - jwt token, token, session, basic auth

--- api/api/urls.py
+++ api/api/urls.py
@@ -36,6 +36,8 @@ schema_view = yasg_get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include("user.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- api/user/urls.py
+++ api/user/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from . import views
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter, Route
 
 router = DefaultRouter()
-router.register(r'users',views.UserViewSet,basename='user')
+
+router.register(r"users", views.UserViewSet, basename="user-create")
+
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
