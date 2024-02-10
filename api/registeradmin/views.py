713a807c2a4c925c2271eb00f68ca1f16024b1dd
aswathi_sivasubramanian <skaswathisk@gmail.com>
2024-02-10 11:52:19 +0530

    Admin App Deleted.

--- api/registeradmin/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Employee
-from .serializers import EmployeeSerializer
-
-class EmployeeViewSet(viewsets.ModelViewSet):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
