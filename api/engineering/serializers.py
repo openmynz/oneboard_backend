726eb65833a96f657d740524203ebaf024b85c95
aswathi_sivasubramanian <skaswathisk@gmail.com>
2024-02-13 11:46:08 +0530

    serializers updated

--- api/engineering/serializers.py
+++ api/engineering/serializers.py
@@ -3,12 +3,14 @@ from .models import Account,Project,User_Project_Mapping
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = '__all__'
+        fields = ['account_name']
+        read_only_fields = ['account_id']
 class ProjectSerializer(serializers.ModelSerializer):
     account_name = serializers.CharField(source='account_id.account_name', read_only=True)
     class Meta:
         model = Project
-        fields = ['project_id', 'project_name','account_id', 'account_name']
+        fields = [ 'project_name','account_id', 'account_name']
+        read_only_fields = ['project_id']
 class User_Project_MappingSerializer(serializers.ModelSerializer):
     class Meta:
         model=User_Project_Mapping
