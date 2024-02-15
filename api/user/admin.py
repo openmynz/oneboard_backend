e79d4c648401b7fc30839f1ba231bbaf00807532
aswathi_sivasubramanian <skaswathisk@gmail.com>
2024-02-15 17:49:47 +0530

    updated typo errors in urls.py

--- api/user/admin.py
+++ api/user/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import LdapUsers,Employee
 # Register your models here.
 admin.site.register(LdapUsers)
-admin.ste.register(Employee)
+admin.site.register(Employee)
 
